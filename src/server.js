c15980ca3e8f3254a71cc5005a1a8d411d2c797a
alineoliveira01 <bernardino.o.aline@gmail.com>
2026-05-16 18:41:39 -0300

    correção pasta server
    mudança do main de index para server

--- src/server.js
+++ src/server.js
@@ -6,17 +6,17 @@ import { fileURLToPath } from 'url';
 import usuarioRoutes from './routes/usuarioRoutes.js';
 import livroRoutes from './routes/livroRoutes.js';
 
-
 dotenv.config();
 
 const app = express();
+
 /* __dirname */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 /* PASTA PUBLIC */
 app.use(express.static(
@@ -28,52 +28,18 @@ app.use('/usuarios', usuarioRoutes);
 
 /* ROTAS LIVROS */
 app.use('/livros', livroRoutes);
+
 /* ROTA PRINCIPAL */
 app.get('/', (req, res) => {
-
     res.sendFile(
         path.join(__dirname, 'public', 'index.html')
     );
 });
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import usuarioRoutes from './routes/usuarioRoutes.js';
-import livroRoutes from './routes/livroRoutes.js';
 
-
-dotenv.config();
-
-const app = express();
-/* __dirname */
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
-
-/* PASTA PUBLIC */
-app.use(express.static(
-    path.join(__dirname, 'public')
-));
-
-/* ROTAS USUÁRIOS */
-app.use('/usuarios', usuarioRoutes);
-
-/* ROTAS LIVROS */
-app.use('/livros', livroRoutes);
-/* ROTA PRINCIPAL */
-app.get('/', (req, res) => {
-
-    res.sendFile(
-        path.join(__dirname, 'public', 'index.html')
-    );
-});
+// Garante que o Render defina a porta automaticamente na nuvem
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+// CONFIGURAÇÃO CORRETA: Adicionado '0.0.0.0' para o Render conseguir conectar
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor rodando com sucesso na porta ${PORT}`);
 });
